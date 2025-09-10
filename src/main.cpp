5d9af19dd0b5dac2b64482e08711edf64486bcaa
sriram18 <sriram.r@surveysparrow.com>
2025-09-10 07:17:10 +0530

    a simple insert select exit and perisitance with dirty pages implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,67 @@
-#include "storage/Pager.h"
-#include "storage/Table.h"
 #include <iostream>
+#include <sstream>
+#include "table/Table.h"
+#include "row/Row.h"
 using namespace std;
 
 int main()
 {
-    Pager pager("mydb.db");
-    Table table(&pager);
+    Table table("data/test.db");
 
-    table.insertRow(Row(1, "alice", "alice@example.com"));
-    table.insertRow(Row(2, "bob", "bob@example.com"));
+    string input;
+    cout << "db > ";
+    while (getline(cin, input))
+    {
+        if (input.empty())
+        {
+            cout << "db > ";
+            continue;
+        }
 
-    pager.flush(0);
+        if (input[0] == '.')
+        {
+            if (input == ".exit")
+            {
+                table.close();
+                cout << "Bye!\n";
+                break;
+            }
+            else
+            {
+                cout << "Unrecognized command: " << input << "\n";
+            }
+        }
+        else
+        {
+            istringstream iss(input);
+            string keyword;
+            iss >> keyword;
 
-    cout << "\nAll rows in DB:\n";
-    table.selectAll();
+            if (keyword == "insert")
+            {
+                int id;
+                string username, email;
+                iss >> id >> username >> email;
+                try
+                {
+                    table.insertRow(Row(id, username, email));
+                }
+                catch (const exception &e)
+                {
+                    cout << "Error: " << e.what() << "\n";
+                }
+            }
+            else if (keyword == "select")
+            {
+                table.selectAll();
+            }
+            else
+            {
+                cout << "Unrecognized keyword at start of input: " << keyword << "\n";
+            }
+        }
+        cout << "db > ";
+    }
 
     return 0;
 }
